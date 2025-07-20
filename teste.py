5d372c35be228486b9791fd77ae0dd80823dff86
Ana Luiza <analu.tibi@outlook.com>
2025-07-20 04:04:42 -0300

    10. Testes com temporizador e Threads

--- teste.py
+++ teste.py
@@ -1,19 +1,27 @@
-import os
+import time
+import threading
 
-# Caminho para o arquivo 'arquivo.txt' (está na mesma pasta do script)
-arquivo_path = os.path.join(os.path.dirname(__file__), 'arquivo.txt')
-
-# Tenta abrir e ler o conteúdo do arquivo
-try:
-    with open(arquivo_path, 'r') as file:
-        conteudo = file.read()
-        print("Conteúdo do arquivo:")
-        print(conteudo)
-except FileNotFoundError:
-    print(f"O arquivo {arquivo_path} não foi encontrado.")
+temp = '3'
 
 
-/projeto
-   /dist
-      meu_programa.exe
-      config.txt
+def tempo():
+    global temp
+    global uaaa
+
+    while temp != '5' or uaaa == '':
+        time.sleep(10)
+        temp = '5'
+        print('{temp}\n')
+        return temp
+    if uaaa == 'config':
+        print('Configuração concluída.')
+        return uaaa
+
+def jessica():
+    global uaaa
+    uaaa = input('Digite algo: ')
+    return uaaa
+
+threading.Thread(target=tempo).start()
+jessica()
+print(uaaa)
