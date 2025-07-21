24186c95a3e294ba3d1951d9cf160069c62b51f0
Ana Luiza <analu.tibi@outlook.com>
2025-07-21 20:47:17 -0300

    12. Biblioteca TimeOut

--- teste.py
+++ teste.py
@@ -4,16 +4,41 @@ import threading
 timeConfig = False
 
 def pausa():
+    global timeConfig
     counter = 0
     while counter < 10:
+        timeConfig = True
         time.sleep(1)
         counter += 1
-    if counter == 10:
-        
         print(counter)
+    if counter == 10:
+        timeConfig = False
+        return timeConfig
+        
+        
 
 def config():
-    input("Email")
-    if 
+    global timeConfig
+    if timeConfig == True:
+        print("Email")
+    elif timeConfig == False:
+        print("False")
+     
+
 threading.Thread(target=pausa).start()
-input("Digite o email")
+config()
+
+
+
+
+#timeout input
+
+from inputimeout import inputimeout, TimeoutOccurred
+
+try:
+    resposta = inputimeout(prompt='Digite seu email: ', timeout=5)
+except TimeoutOccurred:
+    resposta = None
+    print("\nTempo esgotado!")
+
+print(f"Você digitou: {resposta}")
--- timeee.py
+++ timeee.py
@@ -1,43 +1,9 @@
-import os
-import shutil
-import time
+from inputimeout import inputimeout, TimeoutOccurred
 
-initialPath = input("Digite o caminho da pasta inicial: ")
-finalPath = "C:/finalPath"
+try:
+    resposta = inputimeout(prompt='Digite seu email: ', timeout=5)
+except TimeoutOccurred:
+    resposta = None
+    print("\nTempo esgotado!")
 
-initialFolder = os.listdir(initialPath)
-finalFolder = os.listdir(finalPath)
-
-while True: 
-    initialFolder = os.listdir(initialPath)
-    if len(initialFolder) == 0:
-        time.sleep(2 * 60 * 60) 
-    else:
-        for file in initialFolder:
-            local = os.path.join(initialPath, file)
-            moved = False 
-
-            for destinoFile in finalFolder:
-                folderKey = destinoFile.split('[')[0].strip().lower()
-                if folderKey in file.lower():
-                    base, ext = os.path.splitext(file)
-                    renameFile = "[ASSINADO] " + base + ext
-
-                    filePathMatch = os.path.join(finalPath, destinoFile)
-                    filePath = os.path.join(filePathMatch, renameFile)
-                    print(f"Movendo {file} para {filePath}")
-
-                    shutil.move(local, filePath)
-                    moved = True
-                    break 
-
-            if not moved:
-                base, ext = os.path.splitext(file)
-                mkdir = os.path.join(finalPath, base) 
-                os.makedirs(mkdir, exist_ok=True)
-
-                filePath = os.path.join(mkdir, file)
-                print(f"Criando pasta {mkdir} e movendo {file} para lá")
-                shutil.move(local, filePath)
-
-        initialFolder = os.listdir(initialPath)
+print(f"Você digitou: {resposta}")
