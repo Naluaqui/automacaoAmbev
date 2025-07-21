1008b3d48ae675a5804f709506721ab76e46a7fe
Ana Luiza <analu.tibi@outlook.com>
2025-07-21 18:18:25 -0300

    11. Testando configuração de TXT com temporizador

--- teste.py
+++ teste.py
@@ -1,27 +1,19 @@
 import time
 import threading
 
-temp = '3'
+timeConfig = False
 
+def pausa():
+    counter = 0
+    while counter < 10:
+        time.sleep(1)
+        counter += 1
+    if counter == 10:
+        
+        print(counter)
 
-def tempo():
-    global temp
-    global uaaa
-
-    while temp != '5' or uaaa == '':
-        time.sleep(10)
-        temp = '5'
-        print('{temp}\n')
-        return temp
-    if uaaa == 'config':
-        print('Configuração concluída.')
-        return uaaa
-
-def jessica():
-    global uaaa
-    uaaa = input('Digite algo: ')
-    return uaaa
-
-threading.Thread(target=tempo).start()
-jessica()
-print(uaaa)
+def config():
+    input("Email")
+    if 
+threading.Thread(target=pausa).start()
+input("Digite o email")
