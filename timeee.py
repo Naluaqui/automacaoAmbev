f76edb77b2283a6f425cbd3653df91a6154be9d6
Ana Luiza <analu.tibi@outlook.com>
2025-07-23 13:43:09 -0300

    13. Biblioteca Timeout e configuração do arquivo TXT via terminal deu certo

--- timeee.py
+++ timeee.py
@@ -1,7 +1,12 @@
 from inputimeout import inputimeout, TimeoutOccurred
+import os
+configTxt = os.path.join(os.path.dirname(__file__), "arquivo.txt")
 
 try:
-    resposta = inputimeout(prompt='Digite seu email: ', timeout=5)
+    resposta = inputimeout(prompt='Digite seu email: ', timeout=20)
+    if resposta != '':
+        with open(configTxt, "w", encoding="utf=8") as file:
+            file.write(resposta)
 except TimeoutOccurred:
     resposta = None
     print("\nTempo esgotado!")
