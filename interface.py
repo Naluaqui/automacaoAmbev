046f9bd61fa69d9d0092a258549f45ac679c0fd9
Ana Luiza <analu.tibi@outlook.com>
2025-08-16 00:27:44 -0300

    43. Primeiros passos da Interface Oficial

--- interface.py
+++ interface.py
@@ -1,12 +1,46 @@
-import customtkinter
+import customtkinter  as ctk
+import os
+from tkinter import filedialog
 
-app = customtkinter.CTk()
-app.geometry("600x600")
 
-entry = customtkinter.CTkEntry(app, placeholder_text="Adicione o caminho da pasta inicial", width=500)
-entry.pack(padx=20, pady=(20, 20), anchor="w")  # Adicione o entry na janela
+ctk.set_appearance_mode("dark")
 
-button = customtkinter.CTkButton(app, text="my button", fg_color="red")
-button.pack(padx=20, pady=10)
+app = ctk.CTk()
+app.title("Organizador de Arquivos")
+app.geometry("600x300")
+app.resizable(False, False)
+
+def fileExplorer():
+    directory = filedialog.askdirectory(title="Selecione a pasta")
+    if directory:
+        os.startfile(directory)
+
+tittle = ctk.CTkLabel(app, text="Bem-vindo ao Organizador de Arquivos!")
+tittle.pack(pady=10)
+
+frame1 = ctk.CTkFrame(app, fg_color="transparent")
+frame1.pack(pady=10)
+
+tittleInput1 = ctk.CTkLabel(frame1, text="Pasta Inicial:")
+tittleInput1.pack(pady=20, side=ctk.LEFT, padx=10)
+
+label1 = ctk.CTkEntry(frame1, placeholder_text="Digite o caminho da pasta inicial", width=400)
+label1.pack(pady=20, side=ctk.LEFT, padx=2)
+
+button1 = ctk.CTkButton(frame1, text="Achar Pasta", command=fileExplorer)
+button1.pack(pady=20, side=ctk.LEFT, padx=10)
+
+frame2 = ctk.CTkFrame(app, fg_color="transparent")
+frame2.pack(pady=10)
+
+tittleInput2 = ctk.CTkLabel(frame2, text="Pasta FInal:")
+tittleInput2.pack(pady=20, side=ctk.LEFT, padx=10)
+
+label2 = ctk.CTkEntry(frame2, placeholder_text="Digite o caminho da pasta final", width=400)
+label2.pack(pady=20, side=ctk.LEFT, padx=6)
+
+button2 = ctk.CTkButton(frame2, text="Achar Pasta", command=fileExplorer)
+button2.pack(pady=20, side=ctk.LEFT, padx=10)
 
 app.mainloop()
+
